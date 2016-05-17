064b8836ec60d670391bfdd4bf642e2b14a36ca2
Eric Domenzain Morales <ericdomenzain@gmail.com>
2016-05-17 10:20:06 -0500

    Proyecto 1
    Awesome_print, Sqlite3, Depuración Ruby...

---
+++ Proyecto1/ap/permissions.rb
@@ -0,0 +1,36 @@
+require 'awesome_print'
+
+class User
+  attr_reader :personal_info, :contact_info, :role
+
+  def initialize (personal_info, contact_info, role)
+    @personal_info = personal_info
+    @contact_info = contact_info
+    @role = role
+  end
+end
+
+class Role
+  attr_reader :permissions
+
+  def initialize (*permissions)
+    @permissions = permissions
+  end
+
+  def add_permissions (permissions)
+    @permissions << permissions
+  end
+
+  def permissions_for?(key)
+    @permissions.include?key
+  end
+end
+
+admin_personal_info = {'name'=>"Juan",'last_name'=>'Granados', 'birth_year'=>1982}
+admin_contact_info = {'email'=>'ericdomenzain@gmail.com', 'mob'=>'4181138185'}
+admin_role = Role.new('users', 'contacts', 'promotions')
+admin = User.new(admin_personal_info, admin_contact_info, admin_role)
+
+ap admin_personal_info
+ap admin_contact_info
+ap admin_role.methods - Object.methods
---
+++ Proyecto1/sqlite/sqlite_script.rb
@@ -0,0 +1,19 @@
+require 'sqlite3'
+require 'awesome_print'
+
+#Creamos un objeto de base de datos con su nombre y extension
+db = SQLite3::Database.new('users.sqlite3')
+
+#Ejecutamos un script sql en la base de datos
+db.execute <<-SQL
+CREATE TABLE IF  NOT EXISTS users(name varchar(255), email varchar(255), birth_year int)
+SQL
+
+#Ejectuamos una insercion en la tabla usuario
+db.execute 'INSERT INTO users (name, email, birth_year) VALUES (?,?,?)', ['Eric', 'ericdomenzain@gmail.com', 1994]
+
+#Asignamos a la variable usuarios todos los usuarios de la consulta
+users = db.execute 'SELECT * FROM users'
+
+#imprimimos la variable
+ap users
